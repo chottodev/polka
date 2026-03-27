4dd3fc14b5169ac78d9e2dd29d100c2d8a7652b0
sergey <110@mobilon.ru>
2026-03-27 17:06:30 +0700

    fix

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TAG="${TAG:-latest}"
-IMAGE="${IMAGE:-polka}"
+IMAGE="${IMAGE:-antirek/polka}"
 
 docker build \
   -f "${ROOT_DIR}/Dockerfile" \
